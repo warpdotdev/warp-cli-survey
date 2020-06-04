7f528cec677ced86bb5f88394742ffd4e63d135c
Zach Lloyd <zachlloyd@gmail.com>
2020-06-04 17:19:56 -0400

    update installer to clean up

--- install.sh
+++ install.sh
@@ -14,10 +14,12 @@ function run_denver_survey() {
       set -x
       curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.linux.x86_64.tar.gz | tar -xzv dsurvey
       ./dsurvey
+      rm dsurvey
   elif [[ "$OSTYPE" == "darwin"* ]]; then
       set -x
       curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.mac.x86_64.tar.gz | tar -xzv dsurvey
       ./dsurvey
+      rm dsurvey
   else
       set +x
       echo "The Denver Survey installer does not currently work for your platform: $OS"
