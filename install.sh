d5d0e3811eacc856f2a395ca5605ab07e73954df
Zach Lloyd <zachlloyd@gmail.com>
2020-06-08 15:05:29 -0400

    try a different install strategy

--- install.sh
+++ install.sh
@@ -3,31 +3,28 @@
 # Denver Survey installer
 #
 # Usage:
-#   source <(curl -s https://raw.githubusercontent.com/zachlloyd/denver-survey-client/master/install.sh)
+#   curl -s https://raw.githubusercontent.com/zachlloyd/denver-survey-client/master/install.sh | bash && /tmp/dsurvey
 
 VERSION="0.1.3"
 
 set -e
 
 function run_denver_survey() {
+  echo "Installing survey to /tmp/dsurvey"
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
-      # set -x
       curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.linux.x86_64.tar.gz | tar -xzv dsurvey
-      ./dsurvey
-      rm dsurvey
+      mv dsurvey /tmp/dsurvey
+      chmod +x /tmp/dsurvey
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-      #set -x
       curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.mac.x86_64.tar.gz | tar -xzv dsurvey
-      ./dsurvey
-      rm dsurvey
+      mv dsurvey /tmp/dsurvey
+      chmod +x /tmp/dsurvey
   else
       set +x
       echo "The Denver Survey installer does not currently work for your platform: $OS"
       echo "Please let me know at zachlloyd@gmail.com"
       exit 1
   fi
-
-  set +x
 }
 
 run_denver_survey
