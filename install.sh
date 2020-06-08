48b96489cd4753f7aaee8dff308683cd012b5de6
Zach Lloyd <zachlloyd@gmail.com>
2020-06-08 15:08:09 -0400

    silence output of tar

--- install.sh
+++ install.sh
@@ -12,11 +12,11 @@ set -e
 function run_denver_survey() {
   echo "Installing survey to /tmp/dsurvey"
   if [[ "$OSTYPE" == "linux-gnu" ]]; then
-      curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.linux.x86_64.tar.gz | tar -xzv dsurvey
+      curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.linux.x86_64.tar.gz | tar -xzv dsurvey > /dev/null 2>&1
       mv dsurvey /tmp/dsurvey
       chmod +x /tmp/dsurvey
   elif [[ "$OSTYPE" == "darwin"* ]]; then
-      curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.mac.x86_64.tar.gz | tar -xzv dsurvey
+      curl -fsSL https://github.com/zachlloyd/denver-survey-client/releases/download/$VERSION/dsurvey.$VERSION.mac.x86_64.tar.gz | tar -xzv dsurvey > /dev/null 2>&1
       mv dsurvey /tmp/dsurvey
       chmod +x /tmp/dsurvey
   else
